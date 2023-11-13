8e01f23d675af0778ea20c52051370327eb1d702
Melody Ide <m.ide@hotmail.com>
2023-11-13 13:30:13 +0100

    buttons

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,35 @@
-function show() {
+function showlogo() {
     /* Access image by id and change 
     the display property to block*/
     document.getElementById('logos')
         .style.display = "block";
-    document.getElementById('btnID')
+    document.getElementById('projectsbtnhide')
+        .style.display = "block";    
+    document.getElementById('projectsbtn')
+        .style.display = "none";
+}
+
+function hidelogo() {
+    /* Access image by id and change 
+    the display property to block*/
+    document.getElementById('logos')
+        .style.display = "none";
+    document.getElementById('projectsbtnhide')
+        .style.display = "none";    
+    document.getElementById('projectsbtn')
         .style.display = "block";
+}
+
+function showabout() {
+    /* Access image by id and change 
+    the display property to block*/
+    document.getElementById('abouttext')
+        .style.display = "block";
+}
+
+function hideabout() {
+    /* Access image by id and change 
+    the display property to block*/
+    document.getElementById('abouttext')
+        .style.display = "none";
 }
