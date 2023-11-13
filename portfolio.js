4116377715417964e917a3c8487977cbed822328
Melody Ide <m.ide@hotmail.com>
2023-11-13 12:24:27 +0100

    logos ajoutés

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,8 @@
-const function () {
-    
+function show() {
+    /* Access image by id and change 
+    the display property to block*/
+    document.getElementById('logos')
+        .style.display = "block";
+    document.getElementById('btnID')
+        .style.display = "block";
 }
